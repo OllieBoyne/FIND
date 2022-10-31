267932ec0398bd2a82719b1ac3cabce9b3999bdb
Ollie Boyne <ollieboyne@gmail.com>
2022-10-31 15:04:26 +0000

    Added on window resize listener

--- docs/3d-viewer.js
+++ docs/3d-viewer.js
@@ -289,3 +289,14 @@ loadingModelPromise.then(() => {
 	setupModel()
 
 })
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
