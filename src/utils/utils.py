3ce8c9a36d1c25eaf5336c67d463bf42ce01b1b6
OllieBoyne <ollieboyne@gmail.com>
2023-02-09 12:26:42 +0000

    Util referencing fix

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,14 @@
 import init_paths
 import torch
 import yaml
+import os
+
+def up1(p):
+	return os.path.split(p)[0]
+
+FIND_dir = up1(up1(os.path.dirname(os.path.realpath(__file__))))
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-with open('src/cfg.yaml') as infile:
+with open(os.path.join(FIND_dir, 'src/cfg.yaml')) as infile:
 	cfg = yaml.load(infile, Loader=yaml.FullLoader)
